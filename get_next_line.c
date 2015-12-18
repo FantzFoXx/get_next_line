de6de7b7a2cb8c21aa402bb69aa974973e146aa3
Ugo DELORME <udelorme@e3r10p4.42.fr>
2015-12-18 18:38:04 +0100

    bases posees, instable

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,75 @@
 /*   By: udelorme <udelorme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/17 15:32:02 by udelorme          #+#    #+#             */
-/*   Updated: 2015/12/17 18:08:56 by udelorme         ###   ########.fr       */
+/*   Updated: 2015/12/18 18:37:13 by udelorme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdlib.h>
 
-int		get_next_line(int const fd, char **line)
+static t_file	*lst_newfile(int fd)
 {
-	static int readed = 0;
+	t_file *new;
+
+	new = (t_file *)malloc(sizeof(t_file));
+	if (!new)
+		return (NULL);
+	new->fd = fd;
+	new->readed = 0;
+	new->index = 0;
+	ft_memset(new->buf, 0, BUFF_SIZE);
+	new->next = NULL;
+	return (new);
+}
+
+static void		lst_pushfile(t_file **file, int fd)
+{
+	t_file *new;
+	t_file *index;
+
+	index = *file;
+	new = lst_newfile(fd);
+	if (!*file)
+		*file = new;
+	else
+	{
+		while (index->next)
+			index = index->next;
+		index->next = new;
+	}
+}
+
+int				get_next_line(int const fd, char **line)
+{
+	static t_file	*file = NULL;
+	t_file			*bak;
+	char 			*new_line;
+
+	ft_trace("pass0");
+	line = NULL;
+	new_line = NULL;
+	if (fd < 0 || !line)
+	{
+	ft_trace("pass1");
+		return (ERR_RET);
+	}
+	ft_trace("pass12");
+	bak = file;
+	while (fd != file->fd && file)
+		file = file->next;
+	if (!file)
+	{
+		lst_pushfile(&file, fd);
+		bak = file;
+	}
 	
-	readed = read(fd, *line, BUFF_SIZE);
+	while (read(fd, file->buf, BUFF_SIZE))
+	{
+		while (file->buf[file->readed] != ('\n' ^ 0))
+		{
+			ft_trace("pass4");
+		}
+	}
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: udelorme <udelorme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/17 15:50:39 by udelorme          #+#    #+#             */
-/*   Updated: 2015/12/17 18:08:59 by udelorme         ###   ########.fr       */
+/*   Updated: 2015/12/18 18:37:14 by udelorme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,22 @@
 # define GET_NEXT_LINE_H
 
 # define BUFF_SIZE 32
+# define ERR_RET -1
 
 # include <unistd.h>
 # include <fcntl.h>
 # include "libft/libft.h"
 
-int		get_next_line(int const fd, char **line);
+typedef struct	s_file
+{
+	int				fd;
+	int				index;
+	int				readed;
+	char			buf[BUFF_SIZE];
+	struct s_file	*next;
+}				t_file;
+
+
+int				get_next_line(int const fd, char **line);
 
 #endif
